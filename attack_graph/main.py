f83369b25c405e521b500772bdc49f46a78c6cc7
NdA994 <nda994@gmail.com>
2022-04-09 10:58:55 -0700

    creation attack graph

---
+++ attack_graph/main.py
@@ -0,0 +1,35 @@
+import csv
+import pyAgrum as gum
+import pyAgrum.lib.notebook as gnb
+
+vertices = []
+arcs = []
+arcsDict = {}
+invArcsDict = {}
+
+with open('inputData/VERTICES.CSV') as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',')
+  for row in csv_reader:
+    int(row[0])
+    vertices.append(row)
+    
+with open('inputData/ARCS.CSV') as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',')
+  for row in csv_reader:
+    [int(i) for i in row]
+    arcs.append(row)
+    if row[0] in invArcsDict.keys():
+      invArcsDict[row[0]].append(row[1])
+    else:
+      invArcsDict[row[0]] = []
+      invArcsDict[row[0]].append(row[1])
+    arcsDict[row[1]] = row[-1]
+
+bn=gum.BayesNet('BayesianAttackGraph')
+
+for row in vertices:
+  bn.add(gum.LabelizedVariable(row[0],row[1],2))
+for row in arcs:
+  bn.addArc(row[1],row[0])
+
+bn
